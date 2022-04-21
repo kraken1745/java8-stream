b909062c302c662e184ad719e772a5bd0c1b9b3c
aksha <aksha@192.168.26.88>
2022-04-21 10:38:07 +0530

    First Commit

---
+++ GitHubProjectsExample/src/main/java/com/basic/code/HelloWorldExample.java
@@ -0,0 +1,9 @@
+package com.basic.code;
+
+public class HelloWorldExample {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+		System.out.println("I am from Karnataka");
+	}
+
+}
