094dcfcac2bae2197c9476fd59e67e48c2cd5d1a
kraken1745 <87420239+kraken1745@users.noreply.github.com>
2022-04-21 10:05:35 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ class HelloWorld
   public static void main(String args[])
   {
     System.out.println("Hello World");
-    Syste,.out.println("Hi I am fine");
+    System.out.println("Hi I am fine");
     int a,b,c;
     a=10;
     b=29;
